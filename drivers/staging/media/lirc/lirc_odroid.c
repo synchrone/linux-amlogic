b40858557574a933dd517c6e6b1d657db01b2198
Aleksandr Bogdanov <syn@syn.im>
2018-04-15 13:13:07 +0200

    Upgrading lirc_odroid driver to the latest from hardkernel

--- drivers/staging/media/lirc/lirc_odroid.c
+++ drivers/staging/media/lirc/lirc_odroid.c
@@ -1,13 +1,16 @@
 ﻿/*
  * lirc_odroid.c
  *
- * lirc_odroid - Device driver that records pulse- and pause-lengths
- *	      (space-lengths) (just like the lirc_serial driver does)
- *	      between GPIO interrupt events on the Odroid X/X2.
- *	      This was ported from the lirc_rpi driver,
- *	      so I would like say thanks to the authors.
+ * lirc_odroid - Modified version from lirc_gpioblaster.c of OpenWrt.
+ *               taken from https://wiki.openwrt.org/doc/howto/lirc-gpioblaster
  *
- *		  Ported by Xander Victory <xander@victory.id.au>
+ * Copyright (C) 2014 Qball Cow <qball@gmpclient.org>,
+ * Copyright (C) 2012 Aron Robert Szabo <aron@reon.hu>,
+ *                    Michael Bishop <cleverca22@gmail.com>
+ *
+ *  This driver has been modified to support ODROID-C1/C2
+ *  and only supports IR transmitter.
+ *      Modified by Joy Cho <joy.cho@hardkernel.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -18,10 +21,6 @@
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #include <linux/module.h>
@@ -37,12 +36,12 @@
 #include <linux/spinlock.h>
 #include <media/lirc.h>
 #include <media/lirc_dev.h>
-#include <plat/gpio-cfg.h>
 #include <linux/gpio.h>
 
 #define LIRC_DRIVER_NAME "lirc_odroid"
+
 #define RBUF_LEN 256
-#define LIRC_TRANSMITTER_LATENCY 256
+#define LIRC_TRANSMITTER_LATENCY 50
 
 #ifndef MAX_UDELAY_MS
 #define MAX_UDELAY_US 5000
@@ -50,40 +49,23 @@
 #define MAX_UDELAY_US (MAX_UDELAY_MS*1000)
 #endif
 
-#define dprintk(fmt, args...)					\
-	do {							\
-		if (debug)					\
-			printk(KERN_DEBUG LIRC_DRIVER_NAME ": "	\
-			       fmt, ## args);			\
-	} while (0)
-
 /* module parameters */
-
-/* set the default GPIO input pin */
-static int gpio_in_pin = 209;
 /* set the default GPIO output pin */
-static int gpio_out_pin = 203;
+static int gpio_out_pin;
 /* enable debugging messages */
-static int debug;
-/* -1 = auto, 0 = active high, 1 = active low */
-static int sense = -1;
-/* use softcarrier by default */
-static int softcarrier = 1;
+static bool debug;
 
-/*struct gpio_chip *gpiochip;
-struct irq_chip *irqchip;
-struct irq_data *irqdata = 0;*/
+/* softcarrier option : default on */
+static bool softcarrier = 1;
+/* 0 = do not invert output, 1 = invert output */
+static bool invert;
 
 /* forward declarations */
 static long send_pulse(unsigned long length);
 static void send_space(long length);
 static void lirc_odroid_exit(void);
 
-int valid_gpio_pins[] = { 0, 1, 4, 8, 7, 9, 10, 11, 14, 15, 17, 18, 21, 22, 23,
-	24, 25 };
-
 static struct platform_device *lirc_odroid_dev;
-static struct timeval lasttv = { 0, 0 };
 static struct lirc_buffer rbuf;
 static spinlock_t lock;
 
@@ -103,56 +85,69 @@ static void safe_udelay(unsigned long usecs)
 	udelay(usecs);
 }
 
+static unsigned long read_current_us(void)
+{
+	struct timespec now;
+	getnstimeofday(&now);
+	return (now.tv_sec * 1000000) + (now.tv_nsec/1000);
+}
+
 static int init_timing_params(unsigned int new_duty_cycle,
 	unsigned int new_freq)
 {
-	/*
-	 * period, pulse/space width are kept with 8 binary places -
-	 * IE multiplied by 256.
+	/* Time unit of all parameters is microseconds unit.
+	 * It's available for the case when base frequency is smaller
+	 * than 1MHz.
 	 */
-	if (256 * 1000000L / new_freq * new_duty_cycle / 100 <=
-	    LIRC_TRANSMITTER_LATENCY)
-		return -EINVAL;
-	if (256 * 1000000L / new_freq * (100 - new_duty_cycle) / 100 <=
-	    LIRC_TRANSMITTER_LATENCY)
-		return -EINVAL;
 	duty_cycle = new_duty_cycle;
 	freq = new_freq;
-	period = 256 * 1000000L / freq;
+	period = 1000000L / freq;
+
+	/* duty cycle in percentage unit, duty_cycle 50 means 50% */
 	pulse_width = period * duty_cycle / 100;
 	space_width = period - pulse_width;
-	dprintk("in init_timing_params, freq=%d pulse=%ld, "
-		"space=%ld\n", freq, pulse_width, space_width);
+
+	pr_info("in init_timing_params, freq=%d pulse=%ld, space=%ld\n",
+		freq, pulse_width, space_width);
+
 	return 0;
 }
 
 static long send_pulse_softcarrier(unsigned long length)
 {
 	int flag;
-	unsigned long actual, target, d;
-
-	length <<= 8;
+	unsigned long actual, target;
+	unsigned long actual_us, initial_us, target_us;
 
 	actual = 0; target = 0; flag = 0;
+
+	actual_us = read_current_us();
+	/* length from lircd.conf is in us unit */
 	while (actual < length) {
 		if (flag) {
-			gpio_set_value(gpio_out_pin, 0);
+			gpio_set_value(gpio_out_pin, invert);
 			target += space_width;
 		} else {
-			gpio_set_value(gpio_out_pin, 1);
+			gpio_set_value(gpio_out_pin, !invert);
 			target += pulse_width;
 		}
-		d = (target - actual -
-		     LIRC_TRANSMITTER_LATENCY + 128) >> 8;
+
+		initial_us = actual_us;
+		target_us = actual_us + (target - actual);
+
 		/*
 		 * Note - we've checked in ioctl that the pulse/space
 		 * widths are big enough so that d is > 0
 		 */
-		udelay(d);
-		actual += (d << 8) + LIRC_TRANSMITTER_LATENCY;
+		if  ((int)(target_us - actual_us) > 0)
+			udelay(target_us - actual_us);
+
+		actual_us = read_current_us();
+		actual += (actual_us - initial_us);
 		flag = !flag;
 	}
-	return (actual-length) >> 8;
+
+	return actual-length;
 }
 
 static long send_pulse(unsigned long length)
@@ -163,7 +158,7 @@ static long send_pulse(unsigned long length)
 	if (softcarrier) {
 		return send_pulse_softcarrier(length);
 	} else {
-		gpio_set_value(gpio_out_pin, 1);
+		gpio_set_value(gpio_out_pin, !invert);
 		safe_udelay(length);
 		return 0;
 	}
@@ -171,288 +166,50 @@ static long send_pulse(unsigned long length)
 
 static void send_space(long length)
 {
-	gpio_set_value( gpio_out_pin, 0);
+	gpio_set_value(gpio_out_pin, invert);
 	if (length <= 0)
 		return;
 	safe_udelay(length);
 }
 
-static void rbwrite(int l)
-{
-	if (lirc_buffer_full(&rbuf)) {
-		/* no new signals will be accepted */
-		dprintk("Buffer overrun\n");
-		return;
-	}
-	lirc_buffer_write(&rbuf, (void *)&l);
-}
-
-static void frbwrite(int l)
-{
-	/* simple noise filter */
-	static int pulse, space;
-	static unsigned int ptr;
-
-	if (ptr > 0 && (l & PULSE_BIT)) {
-		pulse += l & PULSE_MASK;
-		if (pulse > 250) {
-			rbwrite(space);
-			rbwrite(pulse | PULSE_BIT);
-			ptr = 0;
-			pulse = 0;
-		}
-		return;
-	}
-	if (!(l & PULSE_BIT)) {
-		if (ptr == 0) {
-			if (l > 20000) {
-				space = l;
-				ptr++;
-				return;
-			}
-		} else {
-			if (l > 20000) {
-				space += pulse;
-				if (space > PULSE_MASK)
-					space = PULSE_MASK;
-				space += l;
-				if (space > PULSE_MASK)
-					space = PULSE_MASK;
-				pulse = 0;
-				return;
-			}
-			rbwrite(space);
-			rbwrite(pulse | PULSE_BIT);
-			ptr = 0;
-			pulse = 0;
-		}
-	}
-	rbwrite(l);
-}
-
-static irqreturn_t irq_handler(int i, void *blah, struct pt_regs *regs)
-{
-	struct timeval tv;
-	long deltv;
-	int data;
-	int signal;
-
-	/* use the GPIO signal level */
-	signal = gpio_get_value(gpio_in_pin);
-
-	/* unmask the irq */
-//	irqchip->irq_unmask(irqdata);
-
-	if (sense != -1) {
-		/* get current time */
-		do_gettimeofday(&tv);
-
-		/* calc time since last interrupt in microseconds */
-		deltv = tv.tv_sec-lasttv.tv_sec;
-		if (tv.tv_sec < lasttv.tv_sec ||
-		    (tv.tv_sec == lasttv.tv_sec &&
-		     tv.tv_usec < lasttv.tv_usec)) {
-			printk(KERN_WARNING LIRC_DRIVER_NAME
-			       ": AIEEEE: your clock just jumped backwards\n");
-			printk(KERN_WARNING LIRC_DRIVER_NAME
-			       ": %d %d %lx %lx %lx %lx\n", signal, sense,
-			       tv.tv_sec, lasttv.tv_sec,
-			       tv.tv_usec, lasttv.tv_usec);
-			data = PULSE_MASK;
-		} else if (deltv > 15) {
-			data = PULSE_MASK; /* really long time */
-			if (!(signal^sense)) {
-				/* sanity check */
-				printk(KERN_WARNING LIRC_DRIVER_NAME
-				       ": AIEEEE: %d %d %lx %lx %lx %lx\n",
-				       signal, sense, tv.tv_sec, lasttv.tv_sec,
-				       tv.tv_usec, lasttv.tv_usec);
-				/*
-				 * detecting pulse while this
-				 * MUST be a space!
-				 */
-				sense = sense ? 0 : 1;
-			}
-		} else {
-			data = (int) (deltv*1000000 +
-				      (tv.tv_usec - lasttv.tv_usec));
-		}
-		frbwrite(signal^sense ? data : (data|PULSE_BIT));
-		lasttv = tv;
-		wake_up_interruptible(&rbuf.wait_poll);
-	}
-
-	return IRQ_HANDLED;
-}
-
-static int is_right_chip(struct gpio_chip *chip, void *data)
-{
-	if (!data)
-	{
-		return 0;
-	}
-	
-	int* pin_wanted = (int*)data;
-	
-	dprintk("is_right_chip %s %d\n", chip->label, (*pin_wanted >= chip->base && *pin_wanted < (chip->base + chip->ngpio)));
-
-	if (*pin_wanted >= chip->base && *pin_wanted < (chip->base + chip->ngpio))
-		return 1;
-	return 0;
-}
-
 static int init_port(void)
 {
-	int i, nlow, nhigh, ret, irq;
-
-	//gpiochip = gpiochip_find(&gpio_in_pin, is_right_chip);
-
-	//if (!gpiochip)
-	//	return -ENODEV;
+	int ret;
 
 	if (gpio_request(gpio_out_pin, LIRC_DRIVER_NAME " ir/out")) {
-		printk(KERN_ALERT LIRC_DRIVER_NAME
-		       ": cant claim gpio pin %d\n", gpio_out_pin);
+		pr_info(LIRC_DRIVER_NAME ": cant claim gpio pin %d\n",
+			gpio_out_pin);
 		ret = -ENODEV;
 		goto exit_init_port;
 	}
 
-	if (gpio_request(gpio_in_pin, LIRC_DRIVER_NAME " ir/in")) {
-		printk(KERN_ALERT LIRC_DRIVER_NAME
-		       ": cant claim gpio pin %d\n", gpio_in_pin);
-		ret = -ENODEV;
-		goto exit_gpio_free_out_pin;
-	}
-
-	//dprintk("claimed gpios");
-
-	gpio_direction_input(gpio_in_pin);
-	gpio_direction_output(gpio_out_pin, 1);
-	gpio_set_value(gpio_out_pin, 0);
-
-	s3c_gpio_setpull(gpio_in_pin, S3C_GPIO_PULL_UP);
-
-	//dprintk("set direction");
-
-	//irq = gpiochip->to_irq(gpiochip, gpio_in_pin);
-	//dprintk("to_irq %d\n", irq);
-	//irqdata = irq_get_irq_data(irq);
-
-	//if (irqdata && irqdata->chip) {
-	//	irqchip = irqdata->chip;
-	//} else {
-	//	ret = -ENODEV;
-	//	dprintk("no irqdata/chip  %d\n", (int)irqdata);
-	//	goto exit_gpio_free_in_pin;
-	//}
-
-	/* if pin is high, then this must be an active low receiver. */
-	if (sense == -1) {
-		/* wait 1/2 sec for the power supply */
-		msleep(500);
-
-		/*
-		 * probe 9 times every 0.04s, collect "votes" for
-		 * active high/low
-		 */
-		nlow = 0;
-		nhigh = 0;
-		for (i = 0; i < 9; i++) {
-			if (gpio_get_value(gpio_in_pin))
-				nlow++;
-			else
-				nhigh++;
-			msleep(40);
-		}
-		sense = (nlow >= nhigh ? 1 : 0);
-		printk(KERN_INFO LIRC_DRIVER_NAME
-		       ": auto-detected active %s receiver on GPIO pin %d\n",
-		       sense ? "low" : "high", gpio_in_pin);
-	} else {
-		printk(KERN_INFO LIRC_DRIVER_NAME
-		       ": manually using active %s receiver on GPIO pin %d\n",
-		       sense ? "low" : "high", gpio_in_pin);
-	}
+	gpio_direction_output(gpio_out_pin, invert);
+	gpio_set_value(gpio_out_pin, invert);
 
 	return 0;
 
-	exit_gpio_free_in_pin:
-	gpio_free(gpio_in_pin);
-
-	exit_gpio_free_out_pin:
+exit_init_port:
 	gpio_free(gpio_out_pin);
-
-	exit_init_port:
 	return ret;
 }
 
-// called when the character device is opened
+/* called when the character device is opened */
 static int set_use_inc(void *data)
 {
-	int result;
-	unsigned long flags;
-
-	/* initialize timestamp */
-	do_gettimeofday(&lasttv);
-
-	//request_irq(client->irq, ds3232_irq, IRQF_TRIGGER_FALLING,
-	//			 "ds3232", client);
-	
-	result = request_irq(gpio_to_irq(gpio_in_pin),
-			     (irq_handler_t) irq_handler, IRQF_TRIGGER_FALLING|IRQF_TRIGGER_RISING ,
-			     LIRC_DRIVER_NAME, (void*) 0);
-
-	switch (result) {
-	case -EBUSY:
-		printk(KERN_ERR LIRC_DRIVER_NAME
-		       ": IRQ %d is busy\n",
-		       gpio_to_irq(gpio_in_pin));
-		return -EBUSY;
-	case -EINVAL:
-		printk(KERN_ERR LIRC_DRIVER_NAME
-		       ": Bad irq number or handler, %d\n", gpio_to_irq(gpio_in_pin));
-		return -EINVAL;
-	default:
-		dprintk("Interrupt %d obtained\n",
-			gpio_to_irq(gpio_in_pin));
-		break;
-	};
-
-	/* initialize pulse/space widths */
-	init_timing_params(duty_cycle, freq);
-
-	spin_lock_irqsave(&lock, flags);
-
-	/* GPIO Pin Falling/Rising Edge Detect Enable */
-	//irqchip->irq_set_type(irqdata,
-		//	      IRQ_TYPE_EDGE_RISING | IRQ_TYPE_EDGE_FALLING);
-
-	/* unmask the irq */
-	//irqchip->irq_unmask(irqdata);
-
-	spin_unlock_irqrestore(&lock, flags);
-
+	pr_info(LIRC_DRIVER_NAME " is opened\n");
 	return 0;
 }
 
 static void set_use_dec(void *data)
 {
-	unsigned long flags;
-
-	spin_lock_irqsave(&lock, flags);
-
-	/* GPIO Pin Falling/Rising Edge Detect Disable */
-	//irqchip->irq_set_type(irqdata, 0);
-	//irqchip->irq_mask(irqdata);
-
-	spin_unlock_irqrestore(&lock, flags);
-
-	free_irq(gpio_to_irq(gpio_in_pin), (void *) 0);
-
-	dprintk(KERN_INFO LIRC_DRIVER_NAME
-		": freed IRQ %d\n", gpio_to_irq(gpio_in_pin));
+	pr_info(LIRC_DRIVER_NAME " is closed\n");
 }
 
+/*
+ * Header space pulse
+ * + pre_data space pulse pairs
+ * + key_codes space pulse pairs
+ */
 static ssize_t lirc_write(struct file *file, const char *buf,
 	size_t n, loff_t *ppos)
 {
@@ -469,16 +226,20 @@ static ssize_t lirc_write(struct file *file, const char *buf,
 		return PTR_ERR(wbuf);
 	spin_lock_irqsave(&lock, flags);
 
+	/* refer to space and pulse duration for 'one','zero'
+	   space first and then pulse */
 	for (i = 0; i < count; i++) {
 		if (i%2)
 			send_space(wbuf[i] - delta);
 		else
 			delta = send_pulse(wbuf[i]);
 	}
-	gpio_set_value(gpio_out_pin, 0);
+	/* set default level after transmission */
+	gpio_set_value(gpio_out_pin, invert);
 
 	spin_unlock_irqrestore(&lock, flags);
 	kfree(wbuf);
+
 	return n;
 }
 
@@ -506,8 +267,8 @@ static long lirc_ioctl(struct file *filep, unsigned int cmd, unsigned long arg)
 		break;
 
 	case LIRC_SET_SEND_DUTY_CYCLE:
-		dprintk("SET_SEND_DUTY_CYCLE\n");
 		result = get_user(value, (__u32 *) arg);
+		pr_info(LIRC_DRIVER_NAME " - SET_SEND_DUTY_CYCLE %d\n", value);
 		if (result)
 			return result;
 		if (value <= 0 || value > 100)
@@ -516,8 +277,8 @@ static long lirc_ioctl(struct file *filep, unsigned int cmd, unsigned long arg)
 		break;
 
 	case LIRC_SET_SEND_CARRIER:
-		dprintk("SET_SEND_CARRIER\n");
 		result = get_user(value, (__u32 *) arg);
+		pr_info(LIRC_DRIVER_NAME " - SET_SEND_CARRIER %d\n", value);
 		if (result)
 			return result;
 		if (value > 500000 || value < 20000)
@@ -575,8 +336,7 @@ static int __init lirc_odroid_init(void)
 
 	result = platform_driver_register(&lirc_odroid_driver);
 	if (result) {
-		printk(KERN_ERR LIRC_DRIVER_NAME
-		       ": lirc register returned %d\n", result);
+		pr_info("lirc register returned %d\n", result);
 		goto exit_buffer_free;
 	}
 
@@ -592,13 +352,13 @@ static int __init lirc_odroid_init(void)
 
 	return 0;
 
-	exit_device_put:
+exit_device_put:
 	platform_device_put(lirc_odroid_dev);
 
-	exit_driver_unregister:
+exit_driver_unregister:
 	platform_driver_unregister(&lirc_odroid_driver);
 
-	exit_buffer_free:
+exit_buffer_free:
 	lirc_buffer_free(&rbuf);
 
 	return result;
@@ -606,8 +366,8 @@ static int __init lirc_odroid_init(void)
 
 static void lirc_odroid_exit(void)
 {
+	pr_info(LIRC_DRIVER_NAME "[%s]\n", __func__);
 	gpio_free(gpio_out_pin);
-	gpio_free(gpio_in_pin);
 	platform_device_unregister(lirc_odroid_dev);
 	platform_driver_unregister(&lirc_odroid_driver);
 	lirc_buffer_free(&rbuf);
@@ -615,54 +375,38 @@ static void lirc_odroid_exit(void)
 
 static int __init lirc_odroid_init_module(void)
 {
-	int result, i;
+	int result;
 
 	result = lirc_odroid_init();
 	if (result)
 		return result;
 
-	/* check if the module received valid gpio pin numbers */
-	result = 0;
-	/*if (gpio_in_pin != gpio_out_pin) {
-		for(i = 0; (i < ARRAY_SIZE(valid_gpio_pins)) && (result != 2); i++) {
-			if (gpio_in_pin == valid_gpio_pins[i] ||
-			   gpio_out_pin == valid_gpio_pins[i]) {
-				result++;
-			}
-		}
+	result = init_port();
+	if (result < 0) {
+		pr_info(LIRC_DRIVER_NAME ": init port fail!\n");
+		goto exit_odroid;
 	}
 
-	if (result != 2) {
-		result = -EINVAL;
-		printk(KERN_ERR LIRC_DRIVER_NAME
-		       ": invalid GPIO pin(s) specified!\n");
-		goto exit_rpi;
-	}*/
-
+	/* check if the module received valid gpio pin numbers */
 	driver.features = LIRC_CAN_SET_SEND_DUTY_CYCLE |
 			  LIRC_CAN_SET_SEND_CARRIER |
-			  LIRC_CAN_SEND_PULSE |
-			  LIRC_CAN_REC_MODE2;
+			  LIRC_CAN_SEND_PULSE;
 
 	driver.dev = &lirc_odroid_dev->dev;
 	driver.minor = lirc_register_driver(&driver);
 
 	if (driver.minor < 0) {
-		printk(KERN_ERR LIRC_DRIVER_NAME
-		       ": device registration failed with %d\n", result);
+		pr_info(LIRC_DRIVER_NAME ": device registration failed with %d\n",
+			result);
 		result = -EIO;
-		goto exit_rpi;
+		goto exit_odroid;
 	}
 
-	printk(KERN_INFO LIRC_DRIVER_NAME ": driver registered!\n");
-
-	result = init_port();
-	if (result < 0)
-		goto exit_rpi;
+	pr_info(LIRC_DRIVER_NAME ": driver registered!\n");
 
 	return 0;
 
-	exit_rpi:
+exit_odroid:
 	lirc_odroid_exit();
 
 	return result;
@@ -673,32 +417,24 @@ static void __exit lirc_odroid_exit_module(void)
 	lirc_odroid_exit();
 
 	lirc_unregister_driver(driver.minor);
-	printk(KERN_INFO LIRC_DRIVER_NAME ": cleaned up module\n");
+	pr_info(LIRC_DRIVER_NAME ": cleaned up module\n");
 }
 
 module_init(lirc_odroid_init_module);
 module_exit(lirc_odroid_exit_module);
 
-MODULE_DESCRIPTION("Infra-red receiver and blaster driver for Odroid X/X2 GPIO (based on lirc_rpi).");
-MODULE_AUTHOR("Xander Victory <xander@victory.id.au>");
+MODULE_DESCRIPTION("GPIO based IR Transmitter driver for Odroid C/C2");
+MODULE_AUTHOR("Joy Cho <joy.cho@hardkernel.com>");
 MODULE_LICENSE("GPL");
 
 module_param(gpio_out_pin, int, S_IRUGO);
-MODULE_PARM_DESC(gpio_out_pin, "GPIO output/transmitter pin number of the Exynos processor"
-		 " processor. "//Valid pin numbers are: 0, 1, 4, 8, 7, 9, 10, 11,"
-		 /*" 14, 15, 17, 18, 21, 22, 23, 24, 25,*/" default 203 (GPX1.4, extension header pin 9)");
-
-module_param(gpio_in_pin, int, S_IRUGO);
-MODULE_PARM_DESC(gpio_in_pin, "GPIO input pin number of the Exynos processor. You MUST use an interrupt capable pin!"
-		 /*" Valid pin numbers are: 0, 1, 4, 8, 7, 9, 10, 11, 14, 15,"
-		 " 17, 18, 21, 22, 23, 24, 25,*/" default 209 (GPX2.1, extension header pin 4)");
-
-module_param(sense, bool, S_IRUGO);
-MODULE_PARM_DESC(sense, "Override autodetection of IR receiver circuit"
-		 " (0 = active high, 1 = active low )");
+MODULE_PARM_DESC(gpio_out_pin, "GPIO output/transmitter pin number");
 
 module_param(softcarrier, bool, S_IRUGO);
 MODULE_PARM_DESC(softcarrier, "Software carrier (0 = off, 1 = on, default on)");
+
+module_param(invert, bool, S_IRUGO);
+MODULE_PARM_DESC(invert, "Invert output (0 = off, 1 = on, default off");
 
 module_param(debug, bool, S_IRUGO | S_IWUSR);
 MODULE_PARM_DESC(debug, "Enable debugging messages");
